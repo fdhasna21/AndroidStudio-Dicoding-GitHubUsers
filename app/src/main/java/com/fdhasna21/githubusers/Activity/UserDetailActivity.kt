e9cb247eeed091f0ff6a211b6f3d378d5fe994da
fdhasna21 <fernanda.daymara.hasna@gmail.com>
2021-08-10 21:44:00 +0700

    add intents for share detail

--- app/src/main/java/com/fdhasna21/githubusers/Activity/UserDetailActivity.kt
+++ app/src/main/java/com/fdhasna21/githubusers/Activity/UserDetailActivity.kt
@@ -1,5 +1,9 @@
 package com.fdhasna21.githubusers.Activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -7,15 +11,22 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
+import com.fdhasna21.githubusers.BuildConfig
 import com.fdhasna21.githubusers.DataResolver.User
+import com.fdhasna21.githubusers.DataResolver.getBitmapFromView
 import com.fdhasna21.githubusers.DataResolver.getImageID
 import com.fdhasna21.githubusers.R
 import com.fdhasna21.githubusers.browserIntent
 import kotlinx.android.synthetic.main.activity_user_detail.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class UserDetailActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var user : User
+    private lateinit var user : User
     companion object{
         const val EXTRA_USER = "extra_user"
     }
@@ -53,15 +64,17 @@ class UserDetailActivity : AppCompatActivity(), View.OnClickListener {
                     it?.setOnClickListener(this)
                 }
                 if(idx != 8){
-                    (it as TextView).text = when(idx){
-                        1 -> user.username
-                        2 -> user.name
-                        3 -> user.company
-                        4 -> user.location
-                        5 -> "${user.repository} repositories"
-                        6 -> "${user.follower} followers"
-                        7 -> "${user.following} following"
-                        else -> ""
+                    when(idx){
+                        6   -> detail_followers_count.text = user.follower.toString()
+                        7   -> detail_following_count.text = user.following.toString()
+                        else -> (it as TextView).text = when(idx){
+                            1 -> user.username
+                            2 -> user.name
+                            3 -> user.company
+                            4 -> user.location
+                            5 -> "${user.repository} repositories"
+                            else -> null
+                        }
                     }
                 }
             }
@@ -76,7 +89,27 @@ class UserDetailActivity : AppCompatActivity(), View.OnClickListener {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.menu_share -> {
-                Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
+                try {
+                    val file = File(this.externalCacheDir, "${user.username}.png")
+                    val fout = FileOutputStream(file)
+                    getBitmapFromView(detail_shareable).compress(Bitmap.CompressFormat.PNG, 100, fout)
+                    fout.flush()
+                    fout.close()
+                    file.setReadable(true, false)
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    val photoUri = FileProvider.getUriForFile(applicationContext, BuildConfig.APPLICATION_ID + ".provider", file)
+                    intent.putExtra(Intent.EXTRA_STREAM, photoUri)
+                    intent.putExtra(Intent.EXTRA_TEXT, "See ${user.username} on GitHub via https://github.com/${user.username}")
+                    intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    intent.type = "image/png"
+                    this.startActivity(intent)
+
+                } catch (e : FileNotFoundException){
+                    e.printStackTrace()
+                } catch (e:IOException){
+                    e.printStackTrace()
+                }
             }
         }
         return super.onOptionsItemSelected(item)
@@ -84,9 +117,8 @@ class UserDetailActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when(v?.id){
-            //todo : intent
-            R.id.detail_company -> Toast.makeText(this, "company", Toast.LENGTH_SHORT).show()
-            R.id.detail_location -> Toast.makeText(this, "location", Toast.LENGTH_SHORT).show()
+            R.id.detail_company -> browserIntent("https://www.google.com/search?q=${user.company!!.replace(' ', '+')}", this)
+            R.id.detail_location -> browserIntent("https://www.google.com/maps?q=${user.location!!.replace(' ', '+')}", this)
             R.id.detail_repositories -> browserIntent("https://github.com/${user.username}?tab=repositories", this)
             R.id.detail_followers -> browserIntent("https://github.com/${user.username}?tab=followers", this)
             R.id.detail_following -> browserIntent("https://github.com/${user.username}?tab=following", this)
--- app/src/main/java/com/fdhasna21/githubusers/DataResolver/DrawableUtils.kt
+++ app/src/main/java/com/fdhasna21/githubusers/DataResolver/DrawableUtils.kt
@@ -1,7 +1,27 @@
 package com.fdhasna21.githubusers.DataResolver
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
 
 fun getImageID(imageName: String, context: Context): Int {
     return context.resources.getIdentifier(imageName, "drawable", context.packageName)
+}
+
+@SuppressLint("ResourceAsColor")
+fun getBitmapFromView(view: View) : Bitmap{
+    val output = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(output)
+    val bg = view.background
+
+    if(bg != null){
+        bg.draw(canvas)
+    }
+    else{
+        canvas.drawColor(android.R.color.white)
+    }
+    view.draw(canvas)
+    return output
 }
