e4ca33b2387baebada4fa2fcfa2ea5c67f5b4d94
fdhasna21 <fernanda.daymara.hasna@gmail.com>
2021-08-16 18:03:15 +0700

    viewBinding migration

--- app/src/main/java/com/fdhasna21/githubusers/RowAdapter.kt
+++ app/src/main/java/com/fdhasna21/githubusers/RowAdapter.kt
@@ -8,35 +8,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.fdhasna21.githubusers.dataResolver.User
 import com.fdhasna21.githubusers.dataResolver.getImageID
-import kotlinx.android.synthetic.main.row_recycler.view.*
+import com.fdhasna21.githubusers.databinding.RowRecyclerBinding
 
 class RowAdapter(private val data:ArrayList<User>, val context: Context)
     :RecyclerView.Adapter<RowAdapter.ViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
 
-    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-        val userGH = itemView.row_user!!
-        val usernameGH = itemView.row_username!!
-        val nameGH = itemView.row_name!!
-        val imageGH = itemView.row_image!!
-        val companyGH = itemView.row_company!!
+    class ViewHolder(val binding: RowRecyclerBinding):RecyclerView.ViewHolder(binding.root) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_recycler, parent, false))
+        return ViewHolder(RowRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.usernameGH.text = item.username
-        holder.nameGH.text = item.name
-        holder.companyGH.text = item.company
-        holder.userGH.setOnClickListener {
+        holder.binding.rowUsername.text = item.username
+        holder.binding.rowName.text = item.name
+        holder.binding.rowCompany.text = item.company
+        holder.binding.rowUser.setOnClickListener {
             onItemClickCallback.onItemClicked(data[holder.adapterPosition])
         }
         Glide.with(context)
             .load(getImageID((item.avatar.toString()).substringAfterLast("/"), context))
-            .into(holder.imageGH)
+            .into(holder.binding.rowImage)
     }
 
     override fun getItemCount(): Int {
