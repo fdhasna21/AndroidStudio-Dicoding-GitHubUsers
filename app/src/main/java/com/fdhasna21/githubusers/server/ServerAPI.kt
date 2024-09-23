29f8d234661a6cef89b8ec7097b19d32fd8f46f3
Fernanda Hasna <fernanda.hasna@multipolar.com>
2024-09-23 22:23:45 +0700

    add log retrofit when debug

--- app/src/main/java/com/fdhasna21/githubusers/server/ServerAPI.kt
+++ app/src/main/java/com/fdhasna21/githubusers/server/ServerAPI.kt
@@ -5,37 +5,61 @@ import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
+/**
+ * Updated by Fernanda Hasna on 23/09/2024.
+ */
 class ServerAPI {
     private var retrofit : Retrofit? = null
-    private var httpClient = OkHttpClient.Builder()
 
     fun getServerAPI() : Retrofit{
         if(retrofit == null){
-            httpClient.addInterceptor(Interceptor { chain ->
-                val original: Request = chain.request()
-
-                val request = original.newBuilder()
-                    .header("Accept", "application/vnd.github.v3+json")
-                    .header("Authorization", "client_id ${BuildConfig.GITHUB_CLIENT_ID}")
-                    .method(original.method, original.body)
-                    .build()
-                chain.proceed(request)
-            })
-
             val gson = GsonBuilder()
                 .setLenient()
                 .create()
 
-            val client = httpClient.build()
             retrofit = Retrofit.Builder()
-                .client(client)
+                .client(okHttpClient(interceptor, httpLogging))
                 .baseUrl(BuildConfig.GITHUB_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
         }
         return retrofit!!
+    }
+
+    private val httpLogging: HttpLoggingInterceptor
+        get() {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            return httpLoggingInterceptor
+        }
+
+    private val interceptor = Interceptor { chain ->
+        val original: Request = chain.request()
+        val request = original.newBuilder()
+            .header("Accept", "application/vnd.github.v3+json")
+            .header("Authorization", "client_id ${BuildConfig.GITHUB_CLIENT_ID}")
+            .method(original.method, original.body)
+            .build()
+        chain.proceed(request)
+    }
+
+    private fun okHttpClient(
+        interceptor: Interceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addNetworkInterceptor(httpLoggingInterceptor)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .cache(null)
+            .build()
     }
 }
